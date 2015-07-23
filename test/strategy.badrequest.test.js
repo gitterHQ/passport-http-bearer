d8280198cc54051356efa6c7cd34696db7d4146b
Andrew Newdigate <andrew@troupe.co>
2015-07-23 16:24:14 +0100

    Fixes for duplicate tokens

--- test/strategy.badrequest.test.js
+++ test/strategy.badrequest.test.js
@@ -3,17 +3,17 @@ var chai = require('chai')
 
 
 describe('Strategy', function() {
-    
+
   var strategy = new Strategy(function(token, done) {
-    if (token == 'vF9dft4qmT') { 
+    if (token == 'vF9dft4qmT') {
       return done(null, { id: '1234' }, { scope: 'read' });
     }
     return done(null, false);
   });
-  
+
   describe('handling a request with malformed authorization header', function() {
     var status;
-    
+
     before(function(done) {
       chai.passport(strategy)
         .fail(function(s) {
@@ -25,16 +25,16 @@ describe('Strategy', function() {
         })
         .authenticate();
     });
-    
+
     it('should fail with status', function() {
       expect(status).to.be.a.number;
       expect(status).to.equal(400);
     });
   });
-  
+
   describe('handling a request with token included in more than one way', function() {
     var status;
-    
+
     before(function(done) {
       chai.passport(strategy)
         .fail(function(s) {
@@ -44,15 +44,15 @@ describe('Strategy', function() {
         .req(function(req) {
           req.headers.authorization = 'BEARER vF9dft4qmT';
           req.query = {};
-          req.query.access_token = "vF9dft4qmT";
+          req.query.access_token = "NOTTHESAMETOKEN";
         })
         .authenticate();
     });
-    
+
     it('should fail with status', function() {
       expect(status).to.be.a.number;
       expect(status).to.equal(400);
     });
   });
-  
+
 });
